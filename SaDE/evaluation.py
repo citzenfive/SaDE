6cea8050b87d7fb8d594bf07c9baad254ba77275
João Víctor Costa de Oliveira <joaovictoroliveira1452@gmail.com>
2025-07-22 22:06:51 -0300

    feat: modification in the algorithm stop criteria

--- SaDE/evaluation.py
+++ SaDE/evaluation.py
@@ -4,6 +4,12 @@ from numpy import linalg as lng
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 
+def run_bcim():
+    '''
+    TODO lembrar de modificar o meu código em C para ele já retornar a malha integrada em cada intervalo de tempo
+    '''
+    return
+
 def evaluate(ind):
     result = []
     return result[0],
